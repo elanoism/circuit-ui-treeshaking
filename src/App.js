a839e32600a420e6dea063077c5e2622e0342a6d
Diyan Chuburov <diyan.chuburov@sumup.com>
2018-08-09 09:51:12 +0200

    New usage of themes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import { Card, Button, CardFooter } from "circuit-ui";
 // import Card, { CardFooter } from "circuit-ui/lib/components/Card";
 // import Button from "circuit-ui/lib/components/Button";
-import { circuit } from "circuit-ui/lib/es/themes";
+import { theme } from "circuit-ui";
 import { ThemeProvider } from "emotion-theming";
 
-console.log({ ...circuit });
+console.log(theme);
 
 export const Test = ({ onClick }) => (
   <Card style={{ margin: "20px auto", width: "500px" }}>
@@ -21,7 +21,7 @@ export const Test = ({ onClick }) => (
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={{ ...circuit }}>
+      <ThemeProvider theme={theme.standard}>
         <Test />
       </ThemeProvider>
     );
